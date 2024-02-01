957425d118997de47bd502aaf0eccb37000cc44c
ganesh-b45 <ganeshbellale45@gmail.com>
2024-02-01 19:06:39 +0530

    array

---
+++ merge.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class mergearray
+  {
+    int arr1,arr2;
+    mergearray(int arr1,arr2)
+	{
+	   this.arr1=arr1;
+	   this.arr2=arr2;
+	 }
+	
+	int getmergearray()
+	{
+	  int len1=arr1.length;
+	  int len2=arr2.length;
+	  int len3=len1+len2;
+	  int merge[]=[len3];
+	  
+	 }
+	 public static void main(String args[])
+	 {
+	   
+	 }
