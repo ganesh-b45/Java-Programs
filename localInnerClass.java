000fc03cc6e2b9dac426ff86f3065a775edcbab6
Ganesh Bellale <ganeshbellale45@gmail.com>
2024-02-20 23:03:33 +0530

    task added

--- localInnerClass.java
+++ localInnerClass.java
@@ -1,21 +1,9 @@
-public class localInnerClass
- { 
-   private int data=30;//instance variable 
-   
-   void display()
-   {  
-     class Local  //local class 
-      {  
-        void msg()
-		{
-			System.out.println(data);
-		}  
-      }  
-        Local l=new Local();  
-        l.msg();  
-   }  
- public static void main(String args[]){  
-  localInnerClass obj=new localInnerClass();  
-  obj.display();  
- }  
+public class Parent{
+	public void m1(){
+		System.out.println("hello");
+	}
+    
 }  
+
+public class LocalInnerClass{
+	
